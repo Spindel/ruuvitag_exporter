691ee10d7bf300f84347599a1cdf44934672d6a2
D.S. Ljungmark <spider@skuggor.se>
2023-06-13 12:16:17 +0200

    fixup! Also update metadata on modio backend.

--- src/main.rs
+++ src/main.rs
@@ -195,7 +195,9 @@ mod data {
                     .battery_potential_as_millivolts()
                     .map(f64::from)
                     .map(|num| num / 1000.0),
-                DecidedSensorState::Txpow => self.sensor.tx_power_as_dbm().map(|x| f64::from(x) - 30.0),
+                DecidedSensorState::Txpow => {
+                    self.sensor.tx_power_as_dbm().map(|x| f64::from(x) - 30.0)
+                }
                 DecidedSensorState::Movement => self.sensor.movement_counter().map(f64::from),
                 DecidedSensorState::AccelX => self
                     .sensor
